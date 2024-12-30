c4fbdfeba36804892e58d646b7ab10cf23ddacd1
ryandremza <ryan.dremza@gmail.com>
2024-12-30 12:11:05 +0000

    my profile page - view personal details using reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import {NgModule} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { RouterModule } from "@angular/router";
 import { routes } from "./app.routes";
-import {BrowserModule} from "@angular/platform-browser";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
-import {AuthInterceptor} from "angular-auth-oidc-client";
-import {MyProfileComponent} from "./my-profile/my-profile.component";
-import {UnauthorizedComponent} from "./core/auth/unauthorized/unauthorized.component";
-import {AuthConfigModule} from "./core/auth/auth-config.module";
-import {LoadingSpinnerComponent} from "./core/loading/loading-spinner/loading-spinner.component";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
-import {MatMenuModule} from "@angular/material/menu";
-import {ToolbarComponent} from "./core/nav/toolbar/toolbar.component";
-import {FormsModule} from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "angular-auth-oidc-client";
+import { MyProfileComponent } from "./my-profile/my-profile.component";
+import { UnauthorizedComponent } from "./core/auth/unauthorized/unauthorized.component";
+import { AuthConfigModule } from "./core/auth/auth-config.module";
+import { LoadingSpinnerComponent } from "./core/loading/loading-spinner/loading-spinner.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { ToolbarComponent } from "./core/nav/toolbar/toolbar.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -37,7 +39,10 @@ import {FormsModule} from "@angular/forms";
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
+import {Component, inject, OnInit} from '@angular/core';
+import {map, Observable} from "rxjs";
 import {UserModel} from "../core/auth/services/identity/models/user.model";
 import {IdentityApiFacadeService} from "../core/auth/services/identity/identity-api-facade.service";
+import {FormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-my-profile',
@@ -9,11 +10,26 @@ import {IdentityApiFacadeService} from "../core/auth/services/identity/identity-
   styleUrl: './my-profile.component.css'
 })
 export class MyProfileComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+
   public user$: Observable<UserModel> | undefined;
+  public myProfileForm: UntypedFormGroup = this.formBuilder.group({});
 
   constructor(private readonly identityApiFacadeService: IdentityApiFacadeService) { }
 
   ngOnInit() {
-    this.user$ = this.identityApiFacadeService.getUser();
+    this.user$ = this.identityApiFacadeService.getUser()
+      .pipe(
+        map(x => {
+          this.myProfileForm = this.formBuilder.group({
+            username: [x.userName, Validators.required],
+            email: [x.email, Validators.required],
+            firstName: [x.firstName],
+            lastName: [x.lastName],
+          });
+
+          return x;
+        })
+      )
   }
 }
