e729ca38cd04efce0cecd449b7c684205bb02ff2
ryandremza <ryan.dremza@gmail.com>
2025-03-15 12:03:06 +0000

    move auth code from customer portal into a shared library

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from 'angular-auth-oidc-client';
-import { HmacAuthorizationOverrideInterceptor } from './core/auth/interceptors/hmac-authorization-override.interceptor';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -9,9 +8,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthConfigModule, HmacAuthorizationOverrideInterceptor } from '@falc-platform/shared/auth';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { AuthConfigModule } from './core/auth/auth-config.module';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToolbarComponent } from './core/nav/toolbar/toolbar.component';
---
+++ shared/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/auth/interceptors/hmac-authorization-override.interceptor';
+export * from './lib/auth/auth-config.module';
+export * from './lib/auth/authorization.guard';
